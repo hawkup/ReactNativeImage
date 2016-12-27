a510954302288ab3a6176603626a64e2a9ad9318
spiritsquare <tanarat@bluebik.com>
2016-12-28 00:14:10 +0700

    Build react native from source

--- android/app/src/main/java/com/spiritsquare/reactnativeimage/ReactNativeImage.java
+++ android/app/src/main/java/com/spiritsquare/reactnativeimage/ReactNativeImage.java
@@ -1,20 +1,14 @@
 package com.spiritsquare.reactnativeimage;
 
+
 import android.support.annotation.Nullable;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
-import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.uimanager.SimpleViewManager;
 import com.facebook.react.uimanager.ThemedReactContext;
-import com.facebook.react.uimanager.ViewManager;
-import com.facebook.react.uimanager.ViewProps;
 import com.facebook.react.uimanager.annotations.ReactProp;
-import com.facebook.react.views.image.ImageResizeMode;
-import com.facebook.react.views.image.ReactImageManager;
 import com.facebook.react.views.image.ReactImageView;
-
-import java.util.Arrays;
-import java.util.List;
 
 /**
  * Created by spiritsquare on 12/27/16.
@@ -39,9 +33,10 @@ public class ReactNativeImage extends SimpleViewManager<ReactImageView> {
         return new ReactImageView(context, Fresco.newDraweeControllerBuilder(), mCallerContext);
     }
 
+    // In JS this is Image.props.source
     @ReactProp(name = "src")
-    public void setSrc(ReactImageView view, @Nullable String src) {
-        view.setSource(src);
+    public void setSource(ReactImageView view, @Nullable ReadableArray sources) {
+        view.setSource(sources);
     }
 
     @ReactProp(name = "borderRadius", defaultFloat = 0f)
